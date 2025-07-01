dd4b0b766a8e516bf5a221fbef61a5a0b22aa0a4
Cuneyt <cuneytdonmez8@gmail.com>
2025-07-01 14:26:02 +0300

    fix

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -25,7 +25,7 @@ const Register = () => {
     e.preventDefault();
 
     try {
-      const res = await axios.post(`/api/users/register`, {
+      const res = await axios.post(`http://localhost:3000/api/users/register`, {
         ...form,
         password: encrypt(form.password),
         confirmPassword: encrypt(form.confirmPassword),
